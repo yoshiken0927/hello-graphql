0abfc85d7771f8c588a9fac08d69edec2e010da0
yoshiken0927 <51016793+yoshiken0927@users.noreply.github.com>
2021-01-11 19:02:16 +0900

    step1

---
+++ gqlgen/graph/schema.resolvers.go
@@ -0,0 +1,45 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"gqlgen/graph/generated"
+	"gqlgen/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	return &model.Todo{
+		ID:   "todo001",
+		Text: "部屋の掃除",
+		Done: false,
+		User: &model.User{
+			ID:   "user001",
+			Name: "たろー",
+		},
+	}, nil
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return []*model.Todo{
+		&model.Todo{
+			ID:   "todo001",
+			Text: "部屋の掃除",
+			Done: false,
+			User: &model.User{
+				ID:   "user001",
+				Name: "たろー",
+			},
+		},
+	}, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
